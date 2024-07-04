c846563bfbc2735731b7d7353e5fb1650adf28a7
HTN4KOR <167968062+HTN4KOR@users.noreply.github.com>
2024-07-04 16:52:10 +0530

    Update StringCalculator.h
    testcase1

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,6 +1,8 @@
 int add(const char* input){
     // Check if the string is NULL or the first character is '\0'
-    return (input == NULL || *input == '\0');
+    if(input == NULL || *input == '\0')
+    {
    return 0;
 }
+return -1;
 }
