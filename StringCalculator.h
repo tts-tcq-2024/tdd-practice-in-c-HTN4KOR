4661ffd0d99fe19b62a65769598d249611fac080
HTN4KOR <167968062+HTN4KOR@users.noreply.github.com>
2024-07-04 16:58:06 +0530

    Update StringCalculator.h
    testcase2

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,7 +7,7 @@ int isEmptyString(const char *str) {
 
 int add(const char* input){
     // Check if the string is NULL or the first character is '\0'
-    if(isEmptyString == 1)
+    if(isEmptyString(input))
     {
    return 0;
 }
